1b5d0de7910d4bb0d8584d03c2a8bb3dfa393c1e
Rose Sadler <rosemariesadler@users.noreply.github.com>
2017-10-12 12:52:55 -0600

    Anchored Dialog Support (#2)
    Create fs-dialog-base and separate modal, modeless, and anchored dialogs.
    
    Adds forced direction positioning to anchored dialogs.
    Adds click-away as an opt in to modal dialogs.
    Prevent page scroll on modless drag.
    Add auto positioning, persistent anchored dialogs, and lots of comments.

---
+++ fs-dialog-service.js
@@ -0,0 +1,42 @@
+// service to handle stacking of dialogs
+(function() {
+  var dialogStack = [];
+
+  FS.dialog.service = {};
+
+  FS.dialog.service.addDialogToStack = function(dialogElement) {
+    FS.dialog.service.removeDialogFromStack(dialogElement)
+    dialogStack.push(dialogElement);
+  };
+
+  FS.dialog.service.removeDialogFromStack = function(dialogElement) {
+    var index = dialogStack.indexOf(dialogElement);
+    if (index > -1) dialogStack.splice(index, 1);
+  };
+
+  FS.dialog.service.dialogIsOnTop = function(dialogElement) {
+    var index = dialogStack.indexOf(dialogElement);
+    var lastIndex = dialogStack.length - 1;
+    return index === lastIndex;
+  };
+
+  // not 100% sure if this is needed
+  FS.dialog.service.isDialogInStack = function(dialogElement) {
+    return dialogStack.indexOf(dialogElement) > -1;
+  };
+
+  FS.dialog.service.closeAllDialogs = function() {
+    var reverseStack = dialogStack.reverse();
+    reverseStack.forEach(function(dialog) {
+      dialog.close();
+    })
+  };
+
+  FS.dialog.service.getStack = function() {
+    return dialogStack;
+  };
+
+  FS.dialog.service.windowHasFocus = true;
+  // not 100% sure if this is needed
+  FS.dialog.service.mobileBreakpoint = 480;
+})();
