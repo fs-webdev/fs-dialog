4f30a6665c74a4b9ce93b4913a4882ce12e4c325
Rose Sadler <rosemariesadler@users.noreply.github.com>
2017-10-30 11:43:30 -0600

    Documentation and demo (#10)

--- fs-dialog-service.js
+++ fs-dialog-service.js
@@ -1,8 +1,48 @@
 // service to handle stacking of dialogs
 (function() {
   var dialogStack = [];
+  var buffer = [];
+  var bufferElements = true;
 
-  FS.dialog.service = {};
+  FS = window.FS || {};
+  FS.dialog = FS.dialog || {};
+  FS.dialog.service = FS.dialog.service || {};
+
+  /**
+   * This function will register global elements and attach a reference to them to FS.dialog
+   * @param {string} elementName - The name of the element to register e.g. fs-person-card
+   * @returns {undefined} - Returns void.
+   */
+
+
+  FS.dialog.register = function(elementName) {
+    if (bufferElements) {
+      buffer.push(elementName);
+      return;
+    }
+    registerElement(elementName);
+  };
+
+  function registerElement(elementName) {
+    var camelCaseName = elementName.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+    if (FS.dialog[camelCaseName]) {
+      console.error('Attempted to create element', elementName, 'which already exists');
+      return;
+    }
+    var element = document.createElement(elementName);
+    document.body.appendChild(element);
+    Object.defineProperty(FS.dialog, camelCaseName, {
+      get: function() {
+        return element;
+      }
+    });
+
+    document.addEventListener("DOMContentLoaded", function() {
+      bufferElements = false;
+      buffer.forEach(registerElement)
+    });
+  }
+
 
   FS.dialog.service.addDialogToStack = function(dialogElement) {
     FS.dialog.service.removeDialogFromStack(dialogElement)
@@ -44,10 +84,12 @@
           dialog.setAttribute('transition', animationToUseToClose);
         }
         dialog.close();
-        // Restsore the animation direction after the transition has finished
-        setTimeout(function(){
-          dialog.setAttribute('transition', animationToRestore);
-        }, 300);
+          // Restsore the animation direction after the transition has finished
+        if (animationToUseToClose) {
+          setTimeout(function(){
+            dialog.setAttribute('transition', animationToRestore);
+          }, 300);
+        }
       } else {
         return true;
       }
