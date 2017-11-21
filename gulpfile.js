7c61af142ba057d1d876803f57f367485c78a728
Rose Sadler <rosemariesadler@users.noreply.github.com>
2017-11-21 11:06:26 -0700

    Gulp Build Transpile Task (#32)
    Adds Gulp tasks to build ES5 files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,39 @@
-const gulp = require('gulp');
+const babel = require('gulp-babel');
+const cssSlam = require('css-slam').gulp;
 const fs = require('fs');
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const htmlMinifier = require('gulp-htmlmin');
+const HtmlSplitter = require('polymer-build').HtmlSplitter;
 const glob = require('glob');
+const PolymerProject = require('polymer-build').PolymerProject;
+const project = new PolymerProject({
+  sources: [
+   './fs-anchored-dialog.html',
+   './fs-dialog-base.html',
+   './fs-dialog-positioning-obj.js',
+   './fs-dialog-service.js',
+   './fs-modal-dialog.html',
+   './fs-modeless-dialog.html',
+   './get-root-node-polyfill.js'
+  ],
+});
+const uglify = require('gulp-uglify');
+
 var path = require('path');
 
-gulp.task('build', function(done) {
+gulp.task('watch', function() {
+  gulp.watch('src/*', ['build']);
+});
+
+gulp.task('default', ['build']);
+
+gulp.task('moveFiles', function(done) {
+  return gulp.src(['./src/*', '!./src/fs-dialog-base.html'])
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('injectLocales', ['moveFiles'], function(done) {
   glob('locales/*.json', function(err, files) {
     if (err) done(err);
 
@@ -21,10 +51,6 @@ gulp.task('build', function(done) {
       }
     });
 
-    // console.log(JSON.stringify(langObj,null,2));
-    // done();
-
-    gulp.src(['./src/*', '!./src/fs-dialog-base.html']).pipe(gulp.dest('./'));
     fs.readFile('./src/fs-dialog-base.html', 'utf-8', function(err, file) {
       if (err) done(err);
 
@@ -35,8 +61,24 @@ gulp.task('build', function(done) {
   });
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/*', ['build']);
+gulp.task('build', ['injectLocales'], function(done) {
+  try {
+    const sourcesHtmlSplitter = new HtmlSplitter();
+    const sourcesStream = project.sources()
+      .pipe(sourcesHtmlSplitter.split()) // split inline JS & CSS out into individual .js & .css files
+      .pipe(gulpif(/\.js$/, babel())) // transpile to es5
+      .pipe(gulpif(/\.js$/, uglify())) // minify
+      .pipe(gulpif(/\.css$/, cssSlam())) // minify css (but it may not actually do anything)
+      .pipe(gulpif(/\.html$/, cssSlam())) // there is a bug in polymer-build that makes it so that css doesn't actually get split out - we can still run the css minifier on the html part of the file though.
+      .pipe(gulpif(/\.html$/, htmlMinifier({
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCss: true
+      }))) // minify html
+      .pipe(sourcesHtmlSplitter.rejoin()) // rejoins those files back into their original location
+      .pipe(gulp.dest('./'));
+  } catch (err) {
+    done(err)
+  }
 });
-
-gulp.task('default', ['build']);
