e541f6fd5ee2f0ec606af6d33d90994798410c1a
Rose Marie Sadler <rose.sadler@familysearch.org>
2017-11-30 13:42:40 -0700

    adding uncompiled es6 directory for people who need the uncompiled version to use

--- gulpfile.js
+++ gulpfile.js
@@ -61,29 +61,21 @@ gulp.task('injectLocales', ['moveFiles'], function(done) {
   });
 });
 
-// gulp.task('buildEs6', ['injectLocales'], function(done) {
-//   try {
-//     const sourcesHtmlSplitter = new HtmlSplitter();
-//     const sourcesStreamUncompiled = project.sources()
-//       .pipe(sourcesHtmlSplitter.split()) // split inline JS & CSS out into individual .js & .css files
-//       .pipe(gulpif(/\.css$/, cssSlam())) // minify css (but it may not actually do anything)
-//       .pipe(gulpif(/\.html$/, cssSlam())) // there is a bug in polymer-build that makes it so that css doesn't actually get split out - we can still run the css minifier on the html part of the file though.
-//       .pipe(gulpif(/\.html$/, htmlMinifier({
-//         collapseBooleanAttributes: true,
-//         collapseWhitespace: true,
-//         removeComments: true,
-//         minifyCss: true
-//       }))) // minify html
-//       .pipe(sourcesHtmlSplitter.rejoin()) // rejoins those files back into their original location
-//       .pipe(gulp.dest('./es6'));
-//     done();
-//   } catch (err) {
-//     done(err)
-//   }
-// });
+gulp.task('buildEs6', ['injectLocales'], function(done) {
+  return gulp.src([
+      './fs-anchored-dialog.html',
+      './fs-dialog-base.html',
+      './fs-dialog-positioning-obj.js',
+      './fs-dialog-service.js',
+      './fs-modal-dialog.html',
+      './fs-modeless-dialog.html',
+      './get-root-node-polyfill.js'
+    ])
+    .pipe(gulp.dest('./es6'))
+});
 
-// gulp.task('injectAdapter', ['buildEs6'], function(done) {
-gulp.task('injectAdapter', ['injectLocales'], function(done) {
+gulp.task('injectAdapter', ['buildEs6'], function(done) {
+// gulp.task('injectAdapter', ['injectLocales'], function(done) {
   fs.readFile('./fs-dialog-base.html', 'utf-8', function(err, file) {
     if (err) done(err);
     file = file.replace('<!-- ADAPTER CODE -->', '<script src="../webcomponentsjsv1/custom-elements-es5-adapter.js"></script>');
