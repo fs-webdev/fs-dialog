420544f60ce0e6ba4e64e811786ab6db59cc088a
Clif Bergmann <cbergmann@familysearch.org>
2018-08-16 08:17:32 -0600

    Build: Update configurations
    Travis env vars updated, and corresponding .travis.yml entries removed/updated.
    Add babel-eslint parser to future-proof against esoteric ES6 functions.
    Split WCT browser runs for greater stability.
    Ignore all files except production code in bower:ignore to cut down on install time.

--- gulpfile.js
+++ gulpfile.js
@@ -10,49 +10,49 @@ const replace = require('gulp-replace');
 const PolymerProject = require('polymer-build').PolymerProject;
 const project = new PolymerProject({
   sources: [
-   './fs-anchored-dialog.html',
-   './fs-dialog-base.html',
-   './fs-dialog-positioning-obj.js',
-   './fs-dialog-service.js',
-   './fs-modal-dialog.html',
-   './fs-modeless-dialog.html',
-   './get-root-node-polyfill.js'
-  ],
+    './fs-anchored-dialog.html',
+    './fs-dialog-base.html',
+    './fs-dialog-positioning-obj.js',
+    './fs-dialog-service.js',
+    './fs-modal-dialog.html',
+    './fs-modeless-dialog.html',
+    './get-root-node-polyfill.js'
+  ]
 });
 const uglify = require('gulp-uglify');
 
 var path = require('path');
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('src/*', ['build']);
 });
 
 gulp.task('default', ['build']);
 
-gulp.task('moveFiles', function(done) {
+gulp.task('moveFiles', function (done) {
   return gulp.src(['./src/*', '!./src/fs-dialog-base.html'])
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
 });
 
-gulp.task('build', ['moveFiles'], function(done) {
-  glob('locales/*.json', function(err, files) {
+gulp.task('build', ['moveFiles'], function (done) {
+  glob('locales/*.json', function (err, files) {
     if (err) done(err);
 
     let langObj = {};
 
-    files.forEach(function(file) {
+    files.forEach(function (file) {
       const filePath = path.basename(file, '.json');
-      const lang = filePath.substr(filePath.lastIndexOf('_')+1);
+      const lang = filePath.substr(filePath.lastIndexOf('_') + 1);
 
       try {
         const contents = fs.readFileSync(file, 'utf-8');
         langObj[lang] = JSON.parse(contents);
-      } catch(e) {
+      } catch (e) {
         done(e);
       }
     });
 
-    fs.readFile('./src/fs-dialog-base.html', 'utf-8', function(err, file) {
+    fs.readFile('./src/fs-dialog-base.html', 'utf-8', function (err, file) {
       if (err) done(err);
 
       file = file.replace('/* LANG CODE */', JSON.stringify(langObj));
@@ -61,4 +61,3 @@ gulp.task('build', ['moveFiles'], function(done) {
     });
   });
 });
-
