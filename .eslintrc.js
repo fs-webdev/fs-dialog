00417724743dfed44b8ea638b3df59485fc760e2
Clif Bergmann <cbergmann@familysearch.org>
2019-06-14 08:54:40 -0600

    Add eslint-config-tree configuration
    Add lint:staged option and migrate pre-commit checks to it.
    Convert to .js eslint config files, since we would like to have comments.
    Add modifications to Code Climate configuration to pull in extended eslint configurations properly.
    Update Travis CI configuration to suppress warnings.
    Separate out husky commands and add pre-push hook to run tests (requires manual husky version ^2, until all repositories are migrated).
    
    NOTE: There are a few pre-existing warnings from JSDoc/sonarjs when linting locally. Warnings will not fail linting checks. They are primarily there to help us get used to the new rules.
    NOTE: Developers will need to do a clean install to get all of the latest benefits.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'frontier',
+    'tree'
+  ],
+  plugins: [
+    // Enable plugins that are not natively supported by Code Climate. Otherwise results in build errors.
+    'eslint-plugin-deprecate',
+    'eslint-plugin-sonarjs'
+  ],
+  rules: {
+    'no-invalid-this': 'off'
+  }
+}
